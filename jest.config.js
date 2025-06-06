70d553ca3d90183b51b3f33f1d7afbe80a864bbd
Junaid Aziz <junaidaziz@users.noreply.github.com>
2025-06-06 14:05:12 +0500

    Configure Jest with Babel and fix tests

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,8 @@ export default {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
     '^next/link$': '<rootDir>/test-utils/NextLink.js'
+  },
+  transform: {
+    '^.+\\.js$': 'babel-jest'
   }
 };
