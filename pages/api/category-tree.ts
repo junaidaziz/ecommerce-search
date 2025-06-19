57f2d2565a777c8a160bc60f42430d326efb2145
Junaid Aziz <junaidaziz@users.noreply.github.com>
2025-06-19 22:57:56 +0500

    fix categories api and nextauth

--- pages/api/category-tree.ts
+++ pages/api/category-tree.ts
@@ -1,9 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { getCategoryTree } from '../../lib/products';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method === 'GET') {
-    return res.status(200).json(getCategoryTree());
+    const data = await getCategoryTree();
+    return res.status(200).json(data);
   }
   return res.status(405).json({ message: 'Method Not Allowed' });
 }
