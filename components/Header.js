f86cbe1df253ef49461194b1a9bb2b80cddff24e
Junaid Aziz <junaidaziz@users.noreply.github.com>
2025-06-19 14:38:33 +0500

    Refine brand signup layout and cart badge

--- components/Header.js
+++ components/Header.js
@@ -292,7 +292,7 @@ export default function Header() {
             <div className="relative mr-2">
               <Link
                 href="/cart"
-                className="btn btn-ghost flex items-center gap-1"
+                className="p-2 rounded-full shadow hover:bg-base-200 relative"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
@@ -309,10 +309,9 @@ export default function Header() {
                     d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
                   />
                 </svg>
-                Cart
               </Link>
               {itemCount > 0 && (
-                <span className="badge badge-primary absolute top-0 -right-2 w-5 h-5 flex items-center justify-center rounded-full text-white text-xs">
+                <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full px-1">
                   {itemCount}
                 </span>
               )}
@@ -383,7 +382,7 @@ export default function Header() {
           >
             <li>
               <div className="relative flex items-center">
-                <Link href="/cart" className="flex items-center gap-1">
+                <Link href="/cart" className="p-2 rounded-full shadow hover:bg-base-200 relative">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 24 24"
@@ -399,10 +398,9 @@ export default function Header() {
                       d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
                     />
                   </svg>
-                  Cart
                 </Link>
                 {itemCount > 0 && (
-                  <span className="badge badge-primary absolute top-0 -right-2 w-5 h-5 flex items-center justify-center rounded-full text-white text-xs">
+                  <span className="absolute -top-1 -right-1 bg-red-500 text-white text-xs rounded-full px-1">
                     {itemCount}
                   </span>
                 )}
