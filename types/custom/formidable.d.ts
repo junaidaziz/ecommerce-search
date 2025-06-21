cea8182034c3d141b74ed9e40b44362066274967
Junaid Aziz <junaidaziz@users.noreply.github.com>
2025-06-21 16:36:16 +0500

    refactor: centralize slugify and fix formidable types

--- types/custom/formidable.d.ts
+++ types/custom/formidable.d.ts
@@ -1,1 +1,30 @@
-declare module 'formidable';
+declare module 'formidable' {
+  import { IncomingMessage } from 'http';
+
+  interface File {
+    filepath: string;
+    originalFilename?: string;
+    mimetype?: string;
+    size?: number;
+  }
+
+  export interface Files {
+    [key: string]: File | File[];
+  }
+
+  export interface Fields {
+    [key: string]: any;
+  }
+
+  interface IncomingForm {
+    parse(
+      req: IncomingMessage,
+      callback: (err: any, fields: Fields, files: Files) => void
+    ): void;
+  }
+
+  function formidable(options?: Record<string, any>): IncomingForm;
+
+  export default formidable;
+  export { File };
+}
