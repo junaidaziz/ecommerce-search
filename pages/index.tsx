ac186c90934c620e9de48d490385b8c99b628d24
Junaid Aziz <junaidaziz@users.noreply.github.com>
2025-06-22 17:22:39 +0500

    Remove page-specific header, integrate category slider

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,22 @@
 import Head from 'next/head';
-import HomeHeader from '../components/HomeHeader';
 import HomeHero from '../components/HomeHero';
 import FeaturedProducts from '../components/FeaturedProducts';
 import CategoryPromotion from '../components/CategoryPromotion';
-import Footer from '../components/Footer';
 import { getPageTitle } from '../lib/pageTitle';
 
-const HomePage: React.FC = () => {
+const HomePage: React.FC & { maxWidthClass?: string } = () => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <>
       <Head>
         <title>{getPageTitle('Home')}</title>
       </Head>
-      <HomeHeader />
-      <main className="flex-1">
-        <HomeHero />
-        <FeaturedProducts />
-        <CategoryPromotion />
-      </main>
-      <Footer />
-    </div>
+      <HomeHero />
+      <FeaturedProducts />
+      <CategoryPromotion />
+    </>
   );
 };
+
+HomePage.maxWidthClass = 'max-w-none p-0';
 
 export default HomePage;
