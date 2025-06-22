2368bc104bf775fc3ed37bc558d209e22c8c2411
Junaid Aziz <junaidaziz@users.noreply.github.com>
2025-06-22 11:01:01 +0500

    Refine image layout on product cards and detail page

--- pages/product/[slug].tsx
+++ pages/product/[slug].tsx
@@ -100,7 +100,7 @@ export default function ProductDetail({
       <div className="flex flex-col md:flex-row gap-8">
         <div className="md:w-1/2 flex justify-center">
           <ProductImageSlider
-            className="w-full"
+            className="w-11/12 md:w-10/12 mx-auto"
             images={
               product.images && product.images.length > 0
                 ? product.images
