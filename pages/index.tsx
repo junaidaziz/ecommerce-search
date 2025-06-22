f9d4b0cb3f5b5ba782943251b9efb3348d600dc6
Junaid Aziz <junaidaziz@users.noreply.github.com>
2025-06-22 17:24:52 +0500

    Revert "Refactor homepage layout and add category slider"

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,26 @@
 import Head from 'next/head';
+import HomeHeader from '../components/HomeHeader';
 import HomeHero from '../components/HomeHero';
 import FeaturedProducts from '../components/FeaturedProducts';
 import CategoryPromotion from '../components/CategoryPromotion';
+import Footer from '../components/Footer';
 import { getPageTitle } from '../lib/pageTitle';
 
-const HomePage: React.FC & { maxWidthClass?: string } = () => {
+const HomePage: React.FC = () => {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Head>
         <title>{getPageTitle('Home')}</title>
       </Head>
-      <HomeHero />
-      <FeaturedProducts />
-      <CategoryPromotion />
-    </>
+      <HomeHeader />
+      <main className="flex-1">
+        <HomeHero />
+        <FeaturedProducts />
+        <CategoryPromotion />
+      </main>
+      <Footer />
+    </div>
   );
 };
-
-HomePage.maxWidthClass = 'max-w-none p-0';
 
 export default HomePage;
