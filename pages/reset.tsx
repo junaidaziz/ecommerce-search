93306ca4de2af021447de3c54ef496252776c1fd
Junaid Aziz <junaidaziz@users.noreply.github.com>
2025-06-26 14:27:05 +0500

    fix: allow reset page to scroll

--- pages/reset.tsx
+++ pages/reset.tsx
@@ -21,7 +21,7 @@ const RequestReset: React.FC = () => {
   };
 
   return (
-    <div className="flex min-h-screen items-center justify-center px-4 py-6 sm:px-6 lg:px-8 overflow-hidden">
+    <div className="flex min-h-screen items-center justify-center px-4 py-6 sm:px-6 lg:px-8">
       <Head>
         <title>{getPageTitle('Reset Password')}</title>
       </Head>
