e95c15a8831ebcb20252742b45d284f7e77b2967
Junaid Aziz <junaidaziz@users.noreply.github.com>
2025-06-27 11:15:14 +0500

    feat: show success toast on add to cart

--- components/UI/index.ts
+++ components/UI/index.ts
@@ -1,3 +1,4 @@
 export { default as InputField } from './InputField';
 export { default as GenericInput } from './GenericInput';
 export { default as GenericModal } from '../Modals/GenericModal';
+export { default as Toast } from './Toast';
